c934d1978a09ad914dd8a33461553d2248142c8b
Lindemberg Cortez Gomes Araujo <lindemberg.araujo@guararapes.ind.br>
2020-11-26 22:21:57 -0300

    Implementar botao cadastre-se

--- FrontEnd/src/app/Components/Login/login.component.ts
+++ FrontEnd/src/app/Components/Login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component,EventEmitter, OnInit, Input } from '@angular/core';
+import { Component,EventEmitter, OnInit, Input,ViewChild } from '@angular/core';
 
 import {Usuario, Cliente } from '../../models';
 import { Router } from '@angular/router';
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 
 import {AutenticadorHttpService} from '../../Services/autenticador-http.service';
 import { LoginController } from './login.controller';
-import { Observable } from 'rxjs';
+import { ClienteNewModalComponent } from '../Cliente/cliente-new-modal.component';
 
 @Component({
   selector: 'app-login',
@@ -24,32 +24,52 @@ export class LoginComponent implements OnInit {
   @Input()
   cliente: Cliente;
 
+  NewCliente : Cliente;
+
+
   mostrarLogin :boolean = true;
 
-  constructor(private router: Router, private loginController: LoginController , private autenticadorHttp: AutenticadorHttpService) { }
+  @ViewChild(ClienteNewModalComponent) //pegar uma referencia de um elemento
+  clienteNewModal: ClienteNewModalComponent;
 
-  ngOnInit() {
-    
-  }
+  constructor(private router: Router,
+              private loginController: LoginController ,
+              private autenticadorHttp: AutenticadorHttpService
+              ) { }
+
+  ngOnInit() { this.NewCliente = {Id: 0,
+                        Nome: '',    
+                        Login: '',    
+                        Senha: '', 
+                        Celular: '',  
+                        DataNascimento: null, 
+                        Profissao: '', 
+                        Nacionalidade: 'BRASILEIRO', 
+                        EstadoCivil: '', 
+                        Responsavel: '',
+                        Documento:{}, 
+                        Endereco: {Pais:'BRASIL'},      
+                        Conjuge: {}, 
+                        Banco: {}
+                    }; 
+             }
 
   Login(){
     let ehCliente :boolean =this.usuario.Soucliente==='true';
     console.log(ehCliente);
     if (ehCliente)
     {
-        //let c : Observable<Cliente[]>;
-
-        //c = 
         this.autenticadorHttp.create(this.usuario)
                                  .subscribe(response => { this.cliente=response[0]; 
-                                  console.log(this.cliente);
-                                  if ( this.cliente != undefined)
-                                  {
-                                      this.usuarioAutenticado = true;
-                                      this.mostrarMenuEmitter.emit(false);
-                                      this.router.navigate(['/dashboard/'+this.cliente.Id]);  
-                                      this.mostrarLogin = !this.loginController.usuarioEstaAutenticado();
-                                  }}
+                                                              console.log(this.cliente);
+                                                              if ( this.cliente != undefined)
+                                                              {
+                                                                  this.usuarioAutenticado = true;
+                                                                  this.mostrarMenuEmitter.emit(false);
+                                                                  this.router.navigate(['/dashboard/'+this.cliente.Id]);  
+                                                                  this.mostrarLogin = !this.loginController.usuarioEstaAutenticado();
+                                                              }
+                                                         }
                     );       
     }
     else
@@ -59,6 +79,12 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  Cadastrese()
+  {
+    console.log('cadastrese');
+    this.clienteNewModal.cliente = this.NewCliente; 
+    this.clienteNewModal.show();   
+  }
 }
 
 
